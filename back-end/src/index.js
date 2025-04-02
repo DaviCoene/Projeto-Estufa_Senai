2a0154294d409e232399d484469386e600eb5164
Davi Coene Rosa <62859036+DaviCoene@users.noreply.github.com>
2025-04-02 17:14:02 -0300

    Add files via upload

---
+++ back-end/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors({
+    origin: '*'
+}));
+
+require('./startup/routes')(app);
+
+
+const port = 6666;
+
+const server = app.listen(port, () => console.log(`Listening on port ${port}`));
+
+module.exports = server;
