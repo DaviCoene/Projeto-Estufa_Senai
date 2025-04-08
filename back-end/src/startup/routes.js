c855e1843d39639977b3d1b39b584fb51fe65c36
Davi Coene Rosa <62859036+DaviCoene@users.noreply.github.com>
2025-04-08 17:09:54 -0300

    Add files via upload

--- back-end/src/startup/routes.js
+++ back-end/src/startup/routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const person = require('../routes/person');
+const sensor = require('../routes/sensor');
 
 
 module.exports = function (app) {
     app.use(express.json());
-    app.use('/api/person', person);
+    app.use('/api/sensor', sensor);
 }
